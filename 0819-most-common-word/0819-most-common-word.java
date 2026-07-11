d4522fa0e83ea79ca06829fe3fbf37648bdbf710
Pruthviraj Yewale <rajyewale99@gmail.com>
2026-07-11 17:06:29 +0530

    Time: 16 ms (55.9%), Space: 44.2 MB (79.16%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String stri = paragraph.toLowerCase();
+        System.out.println(stri);
+        String str[] = stri.split("[ ,.!?;']+");
+        HashMap<String,Integer>map = new HashMap<>();
+        for(int i=0; i<str.length; i++){
+            map.put(str[i], map.getOrDefault(str[i],0)+1);
+        }
+        HashSet<String>set = new HashSet<>();
+        for(int i=0; i<banned.length; i++){
+            set.add(banned[i]);
+        }
+        int count = 0;
+        StringBuilder sb = new StringBuilder("");
+        for(Map.Entry<String, Integer> m : map.entrySet()){
+            if(!set.contains(m.getKey()) && m.getValue()>count){
+                count = m.getValue();
+                sb.setLength(0);
+                sb.append(m.getKey());
+            }
+        }
+        return sb.toString();
+    }
+}
